b669d3a575fa8c13c72c8786f961af73faf8965d
erezarbell <38767854+ErezArbell@users.noreply.github.com>
2024-02-16 14:06:17 +0200

    Add dividers.py

---
+++ dividers.py
@@ -0,0 +1,26 @@
+#!python3
+
+# Find the number with the most dividers up to 1000
+
+dividers = {}
+
+def num_of_dividers(n):
+  count = 0
+  for i in range(1, n+1, 1):
+    if n % i == 0:
+      count = count + 1
+  return count
+
+for number in range(1, 1001, 1):
+  num_divs = num_of_dividers(number)
+  print(f"{number}: {num_divs}")
+  dividers[number] = num_divs
+
+max = 0
+index_max = 1
+for index, value in dividers.items():
+  if value > max:
+    max = value
+    index_max = index
+
+print(f"The number {index_max} has the max of {max} dividers")
